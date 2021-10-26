2e41ea12545021c78d083283ae50372244702b2c
marlonsalinas <msal_33@yahoo.com>
2021-10-26 01:30:47 -0500

    changes to nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,14 @@ import { Link } from "react-router-dom";
 function Header(props){
     return (
         <nav className="nav">
+            <Link to='/'>
+                <div>Home</div>
+            </Link>
             <Link to='/astrocollection'>
                 <div>Astrology Collection</div>
+            </Link>
+            <Link to='/nasacollection'>
+                <div>NASA's Collection</div>
             </Link>
         </nav>
     );
