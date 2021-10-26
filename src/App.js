8e7e42b81f9cf1ac251f62a0ed7667d012b7dadb
marlonsalinas <msal_33@yahoo.com>
2021-10-26 01:43:55 -0500

    changes to app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import './App.css';
+import { Route, Switch } from "react-router-dom";
 import Header from "./components/Header";
 import Main from "./components/Main";
+import Home from "./pages/Home";
+import Nasa from "./pages/Nasa";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Main />
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+      </Switch>
+      <Switch>
+        <Route exact path='/astrocollection'>
+          <Main />
+        </Route>
+      </Switch>
+      <Switch>
+        <Route exact path='/nasacollection'>
+          <Nasa />
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/Nasa.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Nasa = (props) => {
+    return <h1>This is the NASA API page</h1>
+};
+
+export default Nasa;
